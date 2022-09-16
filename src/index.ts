2ae51b1997d12d7ea2da947d73c7a8c59bc5bc41
Peter Vacouftsis <reteppetergcz@gmail.com>
2022-09-17 06:29:42 +1000

    Worked on sending a message loading configurations
    However, it seems unable to load the crawler, giving:
    Error: Cannot find module 'evergreen-org-crawler/src/index'

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,67 @@
-import https from 'https';
+require("dotenv").config()
+const https = require("https")
 
-const hostname = '127.0.0.1';
-const port = 3000;
+import {getJsonStructure} from "evergreen-org-crawler/src/index"
 
-const server = https.createServer((req: any, res: any) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-});
+var fs = require("fs");
+var config = JSON.parse(fs.readFileSync("../config.json", "utf8"));
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+export function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
+    return o[propertyName];
+}
+
+async function createData(request: "npm" | "PyPI" | "RubyGems" | null = null){
+	const requestToAPI = {
+		npm: "NPM",
+		PyPI: "PYPI",
+		RubyGems: "RUBYGEMS"
+	}
+
+	let api = null
+	if(request != null){
+		api = [getProperty(requestToAPI, request)]
+	}
+
+	const accessToken = process.env.EVERGREEN_GITHUB_TOKEN!
+	return getJsonStructure(accessToken, config, api)
+}
+
+async function main(){
+	var postData = JSON.stringify({ "text": "Hello, World! (but from a server)" })
+
+	const url = process.env.EVERGREEN_SLACK_WEBHOOK
+	if(!url){
+		console.log("The webhook url must be defined in the .env file \"EVERGREEN_SLACK_WEBHOOK\"=[URL]")
+		process.exit(1)
+	}
+
+	console.log(await createData())
+
+	var options = {
+		hostname: "hooks.slack.com",
+		port: 443,
+		path: "/services/" + url.split("/services/")[1],
+		method: "POST",
+		headers: {
+			"Content-type": "application/json"
+		}
+	}
+
+	var req = https.request(options, (res: any) => {
+		res.on("data", (d: any) => {
+			if(d == "ok"){
+				console.log("Sucessfully sent the message.")
+			} else{
+				console.log("Failed to send the message:")
+				console.log(d)
+			}
+		})
+	}).on("error", (e: any) => {
+		console.error(e)
+	})
+
+	req.write(postData)
+	req.end()
+}
+
+main()
