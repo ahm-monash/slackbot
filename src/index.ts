66719e626f1dd6e10976f31954ac9021df083639
Peter Vacouftsis <reteppetergcz@gmail.com>
2022-09-17 05:04:04 +1000

    Intial setup of NodeJS and npm environment

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import https from 'https';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = https.createServer((req: any, res: any) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
