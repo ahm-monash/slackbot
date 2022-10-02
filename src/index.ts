6a7be66f1cd15f58518259d042e9880ad29c952b
Peter Vacouftsis <reteppetergcz@gmail.com>
2022-10-02 18:07:21 +1100

    Implemented sending a list of outdated dependencies via Slack

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,78 @@
 require("dotenv").config()
 const https = require("https")
 
-import {getJsonStructure} from "evergreen-org-crawler/src/index"
+// @ts-ignore
+import { getJsonStructure } from "evergreen-org-crawler/build/index.js"
+//TODO: Change the above library to export types
+
+//TODO: Move this file to the crawler
+import { semVerFromString, semVerToDelta } from "./semVer"
 
 var fs = require("fs");
-var config = JSON.parse(fs.readFileSync("../config.json", "utf8"));
 
-export function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
-    return o[propertyName];
+//Allows properties to be accessed using strings on objects with TypeScript types
+function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
+	return o[propertyName];
 }
 
-async function createData(request: "npm" | "PyPI" | "RubyGems" | null = null){
+type Config = {
+	targetOrganisation: string,
+	repeatOutdated: boolean
+}
+
+type RawMessageData = Map<
+	string,
+	{
+		current: { name: string, link: string },
+		deps: { latest: { name: string, link: string }, dif: number }[]
+	}[]
+>
+
+//Creates a named hyperlink in the Slack API syntax
+function formatLink(repo: { name: string, link: string }) {
+	return `<${repo.link}|${repo.name}>`
+}
+
+//Adds a header and formats the individual dependencies
+function evergreenMessage(config: Config, rawMsgData: RawMessageData) {
+	const languageMap = {
+		npm: "JavaScript/TypeScript",
+		PyPI: "Python",
+		RubyGems: "Ruby"
+	}
+
+	let msg = "Project *Evergreen* :evergreen_tree:\n_Monitoring the " + config.targetOrganisation + " GitHub organisation_"
+
+	if (!config.repeatOutdated) {
+		msg += "\nNote: Dependencies which were previously reported as outdated are not shown here unless a new major version is released."
+	}
+
+	msg += "\n"
+
+	for (const [language, repos] of rawMsgData) {
+		const languageName = (getProperty(languageMap, language as any) || language)
+		if (repos.length == 0) {
+			msg += "\nAll dependencies of " + languageName + "  repositories are up-to-date!\n"
+		} else {
+			msg += "\nFor repositories written in " + languageName + ":\n"
+			for (const x of repos) {
+				const user = x.current
+				const deps = x.deps
+
+				msg += "\tFor package " + formatLink(user) + ":\n"
+				for (const dep of deps) {
+					msg += "\t\t- Dependency " + formatLink(dep.latest) + " is out of date by " + dep.dif + " majors.\n"
+				}
+				msg += "\n"
+			}
+		}
+	}
+	return { "text": msg }
+}
+
+//Calls the crawler and returns the results
+async function createData(config: Config, request: "npm" | "PyPI" | "RubyGems" | null = null) {
+
 	const requestToAPI = {
 		npm: "NPM",
 		PyPI: "PYPI",
@@ -18,24 +80,127 @@ async function createData(request: "npm" | "PyPI" | "RubyGems" | null = null){
 	}
 
 	let api = null
-	if(request != null){
+	if (request != null) {
 		api = [getProperty(requestToAPI, request)]
 	}
 
-	const accessToken = process.env.EVERGREEN_GITHUB_TOKEN!
-	return getJsonStructure(accessToken, config, api)
+	const accessToken = process.env.GH_TOKEN!
+	return JSON.parse(await getJsonStructure(accessToken, config, api)) //TODO: Once types are fixed, don't call the version that converts to json
 }
 
-async function main(){
-	var postData = JSON.stringify({ "text": "Hello, World! (but from a server)" })
+async function main() {
+	const config = JSON.parse(fs.readFileSync("config.json", "utf8")) as Config;
 
 	const url = process.env.EVERGREEN_SLACK_WEBHOOK
-	if(!url){
+	if (!url) {
 		console.log("The webhook url must be defined in the .env file \"EVERGREEN_SLACK_WEBHOOK\"=[URL]")
 		process.exit(1)
 	}
 
-	console.log(await createData())
+	const data = await createData(config)
+
+	//Internal types of data. data cannot be directly types as field names are unknown
+	type DepsArray = {
+		dep: number,
+		dependencies: [number, string][]
+	}[]
+
+	//Internal types of data. data cannot be directly types as field names are unknown
+	type DepMapType = {
+		name: string,
+		version: string,
+		link: string
+	}
+
+	//Path to the reuslts of the last run, which prevetns repeated out-of-date messages being sent to the users Slack
+	const lastRunPath = "./lastRun.json"
+
+	//Try and load the last run. If this fails, set it to an empty object
+	let inputMap: any
+	try {
+		inputMap = JSON.parse(fs.readFileSync(lastRunPath, "utf8"))
+	} catch (e) {
+		inputMap = {}
+		console.log("Could not find " + lastRunPath + ". If this is not the first run, then a failure has occured.")
+	}
+
+	//Intialise the outpur for this run
+	let outputMap = {
+		aux: {
+			orgName: config.targetOrganisation
+		},
+		data: {} as any
+	}
+
+	//Check if the last run used the same organisation. If it didn't, the previous results are discarded.
+	if (inputMap.aux && (inputMap.aux?.orgName != outputMap.aux.orgName)) {
+		inputMap = {}
+		console.log("Crawling a different organisation that the last run.")
+	}
+
+	//Clear the last run if the user wants us to report all the outdated dependencies anyway
+	if (config.repeatOutdated) {
+		inputMap = {}
+	}
+
+	let rawMsgData: RawMessageData = new Map()
+
+	//For each language/package manager we support...
+	for (const [language, depData] of Object.entries(data)) {
+		if (language == "aux" || !depData) {
+			continue
+		}
+
+		rawMsgData.set(language, [])
+		let curLang = rawMsgData.get(language)!
+
+		const depMap = (depData as [any, any[]])[0]
+		const depsArray = (depData as [any, any[]])[1] as DepsArray
+		if (!depsArray) {
+			continue
+		}
+		//For each module...
+		for (const repo of depsArray) {
+			let addedToMsg: boolean = false;
+
+			//For each dependency
+			for (const [depId, depVersion] of repo.dependencies) {
+				const latest = depMap[depId] as DepMapType
+				const versionDif = semVerToDelta(semVerFromString(depVersion), semVerFromString(latest.version))
+				const user = depMap[repo.dep] as DepMapType
+
+				//Only report as out-of-date if it is two majors behind
+				if (versionDif.major > 1) {
+					const storedName = language + "_" + latest.name + "_" + user.name
+					const last = inputMap?.data ? inputMap?.data[storedName] : undefined
+					outputMap.data[storedName] = { used: depVersion, latest: latest.version }
+					//Only report when a dependency first goes out-of-date, assuming the import hasn't been updated
+					if (last && last.used == depVersion) {
+						const lastDif = semVerToDelta(semVerFromString(last.used), semVerFromString(last.latest))
+						if (lastDif.major <= versionDif.major) {
+							continue
+						}
+					}
+
+					if (!addedToMsg) {
+						curLang.push({ current: { name: user.name, link: user.link }, deps: [] })
+						addedToMsg = true
+					}
+					curLang[curLang!.length - 1].deps.push({ latest: { name: latest.name, link: latest.link }, dif: versionDif.major })
+				}
+			}
+		}
+	}
+
+	//Store the results of this run
+	try {
+		fs.writeFileSync(lastRunPath, JSON.stringify(outputMap))
+	} catch (e) {
+		throw new Error("Could not write to file " + lastRunPath)
+	}
+
+	//Message to send to Slack
+	var postData = JSON.stringify(evergreenMessage(config, rawMsgData))
 
 	var options = {
 		hostname: "hooks.slack.com",
@@ -49,9 +214,9 @@ async function main(){
 
 	var req = https.request(options, (res: any) => {
 		res.on("data", (d: any) => {
-			if(d == "ok"){
+			if (d == "ok") {
 				console.log("Sucessfully sent the message.")
-			} else{
+			} else {
 				console.log("Failed to send the message:")
 				console.log(d)
 			}
---
+++ src/semVer.ts
@@ -0,0 +1,168 @@
+//TODO: Move SemVer, SemVerDelta, semVerToDelta, compareSemVerDelta, semVerToArray, semVerToSkipArray, semVerToString, semVerFromString, and compareWithCutoff to the crawler
+
+export type SemVer = {
+	major: number;
+	minor: number;
+	bug: number;
+	rest: string;
+	skipMinor: boolean;
+	skipBug: boolean;
+};
+
+export type SemVerDelta = {
+	major: number;
+	minor: number;
+	bug: number;
+	skipMinor: boolean;
+	skipBug: boolean;
+};
+
+export function semVerToDelta(current: SemVer, latest: SemVer): SemVerDelta{
+	return {
+		major: latest.major - current.major,
+		minor: Math.max(latest.minor - current.minor, 0),
+		bug: Math.max(latest.bug - current.bug, 0),
+		skipMinor: current.skipMinor,
+		skipBug: current.skipBug
+	}
+}
+
+export function compareSemVerDelta(a: SemVerDelta, b: SemVerDelta): number{
+	if(a.major != b.major){
+		return a.major - b.major
+	} else if (a.minor != b.minor){
+		if(a.skipMinor == b.skipMinor){
+			return a.minor - b.minor
+		} else{
+			let newA = a.skipMinor ? 0 : a.minor;
+			let newB = b.skipMinor ? 0 : b.minor;
+			return newA - newB
+		}
+	} else if (a.bug != b.bug){
+		if(a.skipBug == b.skipBug){
+			return a.bug - b.bug
+		} else{
+			let newA = a.skipBug ? 0 : a.bug;
+			let newB = b.skipBug ? 0 : b.bug;
+			return newA - newB
+		}
+	} else{
+		return 0
+	}
+}
+
+function semVerToArray(semVer: SemVer): number[]{
+	return [semVer.major, semVer.minor, semVer.bug];
+}
+
+function semVerToSkipArray(semVer: SemVer): boolean[]{
+	return [false, semVer.skipMinor, semVer.skipBug];
+}
+
+export function semVerToString(semVer: SemVer): string {
+	let res: string = ""
+	if (semVer.skipMinor) {
+		res += '^'
+	} else if (semVer.skipBug) {
+		res += '~'
+	}
+	res += semVer.major + "." + semVer.minor + "." + semVer.bug
+	if (semVer.rest !== "") {
+		res += "-" + semVer.rest
+	}
+	return res
+}
+
+/*
+* Converts a string to a SemVer (semantic version) object
+* String should be of the form:
+*		[^|=|~|(~>)] *N.N[.N[-.*]]
+* TODO: https://devhints.io/semver
+*/
+export function semVerFromString(semVer: string): SemVer {
+	let skipMinor = false
+	let skipBug = false
+
+	let rubyGemsSkipLast = false
+
+	for(;;){
+		if (semVer[0] == '^') {
+			semVer = semVer.substring(1);
+			skipMinor = true
+			skipBug = true
+		} else if (semVer[0] == '~') {
+			if(semVer[1] == '>'){
+				rubyGemsSkipLast = true
+				semVer = semVer.substring(1)
+			}
+			semVer = semVer.substring(1)
+			skipBug = true
+		} else if (semVer[0] == '=') {
+			semVer = semVer.substring(1)
+		} else if (semVer[0] == '>'){
+			if(semVer[1] == '>'){
+				semVer = semVer.substring(1)
+			}
+			semVer = semVer.substring(1)
+			skipMinor = true
+			skipBug = true
+		}else{
+			break
+		}
+	}
+
+	const parts = semVer.split(".", 3)
+
+	if (parts.length == 2) {
+		return {
+			major: parseInt(parts[0]),
+			minor: parseInt(parts[1]),
+			bug: 0,
+			rest: "",
+			skipMinor: rubyGemsSkipLast || skipMinor,
+			skipBug: true
+		}
+	}
+
+	if (parts.length < 3) {
+		return {
+			major: 0,
+			minor: 0,
+			bug: 0,
+			rest: "",
+			skipMinor: skipMinor,
+			skipBug: skipBug
+		}
+	}
+
+	const bugAndRest = parts[2].split("-")
+	//Handle negative number
+	if(bugAndRest[0] == ""){
+		bugAndRest.shift();
+		bugAndRest[0] = '-' + bugAndRest[0];
+	}
+
+	return {
+		major: parseInt(parts[0]),
+		minor: parseInt(parts[1]),
+		bug: parseInt(bugAndRest[0]),
+		rest: bugAndRest[1] ? bugAndRest[1] : "",
+		skipMinor: skipMinor,
+		skipBug: rubyGemsSkipLast || skipBug
+	}
+}
+
+function compareWithCutoff(used: SemVer, current: SemVer, allowance: SemVer, ignoreSkips: boolean = true): boolean{
+	const U = semVerToArray(used);
+	const S = semVerToSkipArray(used);
+	const C = semVerToArray(current);
+	const A = semVerToArray(allowance);
+
+	let pass = true;
+	for(let i = 0; i != U.length; ++i){
+		if(A[i] == -1){ continue; }
+		pass &&= (U[i] + A[i] >= C[i]) || (S[i] && ignoreSkips);
+	}
+
+	return pass;
+}
